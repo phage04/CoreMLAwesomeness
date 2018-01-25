3d3cfbcb33c2ca455d6360f31d7339568be97742
lylejover <lylejover2yahoo.com>
2018-01-25 16:38:40 -0700

    completed v1

--- CoreMLAwesomeness/CoreMLAwesomeness/Controller/ImageVC.swift
+++ CoreMLAwesomeness/CoreMLAwesomeness/Controller/ImageVC.swift
@@ -61,9 +61,13 @@ class ImageVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataS
     
     func handleResults(request: VNRequest, error: Error?){
         guard let results = request.results as? [VNClassificationObservation] else {return}
-        for result in results {
-            print(result.identifier)
-        }
+        let bestResult = results[0]
+        let id = bestResult.identifier.capitalized
+        let confidence = bestResult.confidence * 100
+        let confidenceDecimal = String.init(format: "%.2f", confidence)
+        print(id, "\(confidenceDecimal)%")
+        
+        bottomLabel.text = "\(id): \(confidenceDecimal)%"
     }
 
 }
